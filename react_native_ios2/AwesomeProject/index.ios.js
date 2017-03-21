98b331fc7b4cb9459288e97a98087697675644c6
nownabe <nownabe@gmail.com>
2017-03-21 19:02:31 +0900

    [React Native] State

--- react_native_ios2/AwesomeProject/index.ios.js
+++ react_native_ios2/AwesomeProject/index.ios.js
@@ -11,19 +11,30 @@ import {
   View
 } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
   render() {
-    return <Text>Hello {this.props.name}!</Text>;
+    let display = this.state.showText ? this.props.text : ' ';
+    return <Text>{display}</Text>;
   }
 }
 
 export default class AwesomeProject extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
