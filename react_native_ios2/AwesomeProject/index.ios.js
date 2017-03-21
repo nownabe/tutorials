626c9b73fa4d9874f89563177a690c8b5f2dff89
nownabe <nownabe@gmail.com>
2017-03-21 18:53:09 +0900

    props

--- react_native_ios2/AwesomeProject/index.ios.js
+++ react_native_ios2/AwesomeProject/index.ios.js
@@ -8,12 +8,23 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
+  View
 } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return <Text>Hello {this.props.name}!</Text>;
+  }
+}
 
 export default class AwesomeProject extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
