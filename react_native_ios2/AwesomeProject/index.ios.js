26ac0dea55b3642bbb844205106d9846f862598f
nownabe <nownabe@gmail.com>
2017-03-21 18:41:08 +0900

    React Native Tutorial

---
+++ react_native_ios2/AwesomeProject/index.ios.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
